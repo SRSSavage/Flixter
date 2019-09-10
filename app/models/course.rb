c746477e434e3b92cd5ef1336e90805860ba5dc8
Zach Hewitt <zach2487@gmail.com>
2019-09-10 21:37:33 +0000

    new repo

---
+++ app/models/course.rb
@@ -0,0 +1,20 @@
+class Course < ApplicationRecord
+  mount_uploader :image, ImageUploader
+    
+	has_many :enrollments
+  belongs_to :user
+  has_many :sections
+    
+
+  validates :title, presence: true
+  validates :description, presence: true 
+  validates :cost, presence: true, numericality: {greater_or_equal_to: 0}
+    
+  def free?
+		cost.zero?
+	end
+
+	def premium?
+		! free?
+	end
+end
